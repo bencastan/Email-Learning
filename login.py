99346fcb564f3afac473f3b97b52985feff6a5f2
Ben Castan <ben@castanfamily.com>
2018-02-02 17:46:37 +1100

    Added login.py using u/n & p/w but unencryppted

--- login.py
+++ login.py
@@ -22,13 +22,11 @@ in Foundations of Python Network Programming.
 def main():
     if len(sys.argv) < 4:
         name = sys.argv[0]
-        print("Usage: () server fromaddr toaddr [toaddr...]".format(name))
+        print("Usage: {} server fromaddr toaddr [toaddr...]".format(name))
         sys.exit(2)
 
     server , fromaddr, toaddrs = sys.argv[1], sys.argv[2], sys.argv[3:]
     message = message_template.format(', '.join(toaddrs), fromaddr)
-
-
     username = input("Enter username")
     password = getpass("Enter password)")
 
@@ -45,7 +43,7 @@ def main():
         print(e)
         sys.exit(1)
     else:
-        s = '' if len(toaddrs) ==1  else 's'
+        s = '' if len(toaddrs) == 1  else 's'
         print("Message sent to {} recipient{}".format(len(toaddrs), s))
         connection.quit()
 
